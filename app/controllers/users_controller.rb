33ed7ca5ce8150697e38ca86d3f805c8cbd1812d
檜澤ゆい <hizawayui@gmail.com>
2020-06-21 13:52:57 +0900

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def index
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
   	@book = Book.new #new 
-    @user = current_user
+    
   end
 
   def edit
@@ -26,6 +26,18 @@ class UsersController < ApplicationController
   	else
   		render :edit
   	end
+  end
+
+  def following
+      @user  = User.find(params[:id])
+      @users = @user.followings
+      render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about'
   devise_for :users
   root to: 'home#top'
-  resources :users#,only: [:show,:index,:edit,:update]
+
+  resources :users do
+  	member do
+  		get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resources :book_comments, only: [:create, :destroy]
   end
-
 
 end
